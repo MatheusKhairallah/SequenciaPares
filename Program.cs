1a73052c2c2b4ec18075b9b1fc02fc66ade44672
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-07-03 21:19:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite qualquer número inteiro: ");
+            int numero=Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            
+            int contador=0;
+            while (contador<=numero)
+            {
+              Console.Write($"{contador} ");
+               contador=contador+2; 
+            }
+               Console.WriteLine("Programa finalizado!!!");
+            
+            
+        }
+    }
+}
